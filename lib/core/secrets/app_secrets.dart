32773e704024669e50fcb8ffd531523bfebb5022
JagdishSingh01 <jatinpawar354@gmail.com>
2025-06-08 01:34:17 +0530

    supabase setup,datalayer-authremote datasource and datalayer-authrepository implementation feature added

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://copmdpqabxiwtyaocyzk.supabase.co';
+  static const supabaseAnnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvcG1kcHFhYnhpd3R5YW9jeXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMDg1MTksImV4cCI6MjA2NDg4NDUxOX0.x1wMRQurbF0lgfmvlDPpxq8w6RXbjfhOw-RkLEmzc1k';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnnonKey,
+  );
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,8 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Blog app',
       theme: AppTheme.darkThemeMode,
-      home:LoginPage(),
+      home: LoginPage(),
     );
   }
 }
-
