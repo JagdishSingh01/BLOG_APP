33616827163af0ff489fbaef94cefe67a4298730
JagdishSingh01 <jatinpawar354@gmail.com>
2025-06-12 21:45:58 +0530

    user entity & user model feature added

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,7 +2,8 @@ import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
-import 'package:blog_app/features/auth/domain/usecases/user_signUp.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_signup.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -19,17 +20,27 @@ Future<void> initDependencies() async {
 }
 
 void _initAuth() {
-  serviceLocator.registerFactory<AuthRemoteDataSource>(
+  //datasource
+  serviceLocator
+  ..registerFactory<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(serviceLocator()),
-  );
-
-  serviceLocator.registerFactory<AuthRepository>(
+  )
+  //repository
+  ..registerFactory<AuthRepository>(
     () => AuthRepositoryImpl(serviceLocator()),
-  );
-
-  serviceLocator.registerFactory(() => UserSignup(serviceLocator()));
-
-  serviceLocator.registerLazySingleton(
-    () => AuthBloc(userSignup: serviceLocator()),
+  )
+  //usecases
+  ..registerFactory(
+    () => UserSignup(serviceLocator()),
+  )
+  ..registerFactory(
+    () => UserLogin(serviceLocator()),
+  )
+  //bloc
+  ..registerLazySingleton(
+    () => AuthBloc(
+      userSignup: serviceLocator(),
+      userLogin: serviceLocator()
+    ),
   );
 }
